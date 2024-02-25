1498c90ae6f2c9429707b3a905833d1d1934d5d5
Cyrildickson <cyrildickson147@gmail.com>
2024-02-25 20:20:44 +0000

    first push

---
+++ public/js/script.js
@@ -0,0 +1,41 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     let slides = document.querySelectorAll('.slide');
+//     let index = 0;
+
+//     function nextSlide() {
+//         slides[index].style.display = 'block';
+//         index = (index + 1);
+//         slides[index].style.display = 'block';
+//     }
+
+//     setInterval(nextSlide, 1000); // Change image every 3 seconds
+// });
+
+// TW Elements is free under AGPL, with commercial license required for specific uses. See more details: https://tw-elements.com/license/ and contact us for queries at tailwind@mdbootstrap.com 
+// Initialization for ES Users
+import {
+    Ripple,
+    Input,
+    initTE,
+  } from "tw-elements";
+  
+  initTE({ Ripple, Input });
+
+  // TW Elements is free under AGPL, with commercial license required for specific uses. See more details: https://tw-elements.com/license/ and contact us for queries at tailwind@mdbootstrap.com 
+// Initialization for ES Users
+import {
+    Lightbox,
+    initTE,
+  } from "tw-elements";
+  
+  initTE({ Lightbox });
+
+
+  // TW Elements is free under AGPL, with commercial license required for specific uses. See more details: https://tw-elements.com/license/ and contact us for queries at tailwind@mdbootstrap.com 
+// Initialization for ES Users
+import {
+    Carousel,
+    initTE,
+  } from "tw-elements";
+  
+  initTE({ Carousel });
